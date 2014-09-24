67093b05810fbaade0f6c53baa5671820ab274fc
Tairy <tairyguo@gmail.com>
2014-09-24 23:24:19 +0800

    go to sleep

--- class-tinysou-plugin.php
+++ class-tinysou-plugin.php
@@ -9,16 +9,69 @@
 */
 
 class TinysouPlugin {
+	private $client = NULL;
+	private $document_type_slug = 'posts';
+
+	private $api_key = NULL;
+	private $engine_slug = NULL;
+	private $engine_name = NULL;
+	private $engine_key = NULL;
+	private $num_indexed_documents = 0;
+	private $api_authorized = false;
+	private $engine_initialized = false;
+
+	private $post_ids = NULL;
+	private $total_result_count = 0;
+	private $num_pages = 0;
+	private $per_page = 0;
+	private $search_successful = false;
+	private $error = NULL;
+	private $results = NULL;
+
+	private $max_retries = 5;
+	private $retry_delay = 2;
+
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'tinysou_menu' ) );
+
+		if ( ! is_admin() ){
+			$this->initialize_api_client();
+		}
 	}
 
 	public function tinysou_admin_page(){
-		//include( 'tinysou-admin-page.php' );
-		echo "Admin Page Test";
+		include( 'tinysou-admin-page.php' );
 	}
 
 	public function tinysou_menu() {
 		add_menu_page( '微搜索', '微搜索', 'manage_options', "tinysou", array( $this, 'tinysou_admin_page' ), plugins_url( 'assets/logo.png', __FILE__ ));
+	}
+
+	public function initialize_api_client() {
+		$this->api_key = get_option( 'tinysou_api_key' );
+		$this->engine_slug = get_option( 'tinysou_engine_slug' );
+		$this->engine_key = get_option( 'tinysou_engine_key' );
+
+		$this->client = new TinysouClient();
+		$this->client->set_api_key( $this->api_key );
+	}
+
+	public function check_api_authorized(){
+		if( ! is_admin() )
+			return;
+		if( $this->api_authorized )
+			return;
+
+		if( $this->api_key && strlen( $this->api_key ) > 0 ) {
+			try {
+				$this->api_authorized = $this->client->authorized();
+			} catch( TinysouError $e ) {
+				$this->api_authorized = false;
+			} else {
+				$this->api_authorized = false;
+			}
+
+			update_option( 'tinysou_api_authorized', $this->api_authorized );
+		}
 	}
 }
--- tinysou-authorize.php
+++ tinysou-authorize.php
@@ -1,25 +1,23 @@
-<!-- <div class="wrap">
-	<h2 class="swiftype-header">Swiftype Search Plugin</h2><br/>
-	<form name="swiftype_settings" method="post" action="<?php echo esc_url( $_SERVER['REQUEST_URI'] ); ?>">
-		<?php wp_nonce_field('swiftype-nonce'); ?>
-		<input type="hidden" name="action" value="swiftype_set_api_key">
+<div class="wrap">
+	<h2 class="tinysou-header">微搜索插件</h2><br/>
+	<form name="tinysou_settings" method="post" action="<?php echo esc_url( $_SERVER['REQUEST_URI'] ); ?>">
+		<?php wp_nonce_field('tinysou-nonce'); ?>
+		<input type="hidden" name="action" value="tinysou_set_api_key">
 		<table class="widefat" style="width: 650px;">
 			<thead>
 				<tr>
-					<th class="row-title">Authorize the Swiftype Search Plugin</th>
+					<th class="row-title">认证插件</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
 					<td>
-						Thanks for installing the Swiftype Search Plugin for Wordpress. Please enter your Swiftype API key in the field below and click 'Authorize' to get started.
-						If you don't have an API Key, you can get one by signing up for a free account at <a href="http://swiftype.com/users/sign_up" target="_new">swiftype.com</a>.
-						You will find your API Key at the top of the Swiftype <b>Account Settings</b> screen.<br/><br/>
+						感谢使用为搜索插件，请在下面输入您的appid
 						<ul>
 							<li>
-								<label>Swiftype API Key:</label>
+								<label>Tinysou API Key:</label>
 								<input type="text" name="api_key" class="regular-text" />
-								<input type="submit" name="Submit" value="Authorize" class="button-primary" />
+								<input type="submit" name="Submit" value="认证" class="button-primary" />
 							</li>
 					</td>
 				</tr>
@@ -27,5 +25,4 @@
 		</table>
 	</form>
 
-</div> -->
-<h1>haha</h1>
+</div>
